25a9858eb918108f522dd6dc78ab144c06e51412
aroup <aroupdhruba@gmail.com>
2017-04-22 19:23:33 +0600

    Added corner css

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,25 +3,6 @@ import React,{Component} from 'react';
 import '../styles/card.css'
 import { Tag , Button} from 'antd';
 
-// class Result extends Component {
-//   render(){
-//     return (
-//
-//         <Card style={{ width: 200 }} bodyStyle={{ padding: 10 }}>
-//            <div className="custom-image">
-//              <img alt="example" width="100%" src={this.props.images[0]} />
-//            </div>
-//            <div className="custom-card">
-//              <p>{this.props.title}</p>
-//              <p>{this.props.price}</p>
-//            </div>
-//          </Card>
-//       </div>
-//
-//     )
-//   }
-// }
-
 class Result extends Component {
   handleOnClick=()=>{
         this.props.actions.addCart({
@@ -33,14 +14,16 @@ class Result extends Component {
     var price = this.props.price/100;
     return (
       <div className="floating-box">
-        <div>
-          <img alt="example" width="250" height="250" src={this.props.images[0]}/>
+        <div className="container">
+            <div className="topright">
+              <Tag color="#f50">${price}</Tag>
+            </div>
+            <img alt="example" width="250" height="300" src={this.props.images[0]}/>
         </div>
         <b>{this.props.title}</b>
         <br/>
-        <Tag color="#f50">${price}</Tag>
+        <br/>
         <Button type="primary" onClick={this.handleOnClick}>Add To Cart!</Button>
-        {/* <p> {this.props.asin}</p> */}
     </div>
     )
   }
